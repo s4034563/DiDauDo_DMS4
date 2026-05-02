8da90e7760965cd025cd8994a8fcef717b2dd58b
Do Thai Hung <s4034563@rmit.edu.vn>
2026-05-03 02:18:23 +0700

    Fix Cloudinary config to use window.VIBEMAP_CONFIG instead of import.meta.env

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 window.VIBEMAP_CONFIG = {
     useConvex: true,
-    convexBaseUrl: 'https://spotted-mockingbird-143.convex.site'
+    convexBaseUrl: 'https://spotted-mockingbird-143.convex.site',
+    cloudinaryCloudName: 'dnstelkfm',
+    cloudinaryUploadPreset: 'didaudo_uploads'
 };
